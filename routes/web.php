3e93a1c73ccddee619fe1dd629cf7631c542bf47
suzuki071 <83170397+suzuki071@users.noreply.github.com>
2024-02-09 15:48:04 +0900

    laravel入門 chapter34 継承レイアウト

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('hello/{id?}', [HelloController::class, 'index']); //ルートパラ�
 Route::get('hello2', [HelloController::class, 'index2']);
 Route::get('hello3', [HelloController::class, 'index3']);
 Route::post('hello3', [HelloController::class, 'post3']);
+Route::get('hello4', [HelloController::class, 'index4']);
 
 Route::get('/user',[UserController::class, 'index']);
 Route::get('/user/confirm',[UserController::class, 'confirm']);
